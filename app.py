224426f5c0e3d9dc7d37eaef5a863704d35e44d4
AnviVerma27 <anviver2005@gmail.com>
2023-07-25 21:06:16 +0530

    streamlit app

--- app.py
+++ app.py
@@ -3,31 +3,28 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-def main():
-    st.title("Webcam Streamlit App")
 
-    cap = cv2.VideoCapture(0)
+st.title("Webcam Streamlit App")
 
-    if not cap.isOpened():
-        st.error("Error: Unable to access the webcam.")
-        return
-    frame_width = 640
-    frame_height = 480
-    cap.set(3, frame_width)
-    cap.set(4, frame_height)
-    placeholder = st.empty()
-    while True:
-        ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        if not ret:
-            st.error("Error: Unable to capture frame.")
-            break
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        placeholder.image(rgb_frame, channels='RGB')
-    cap.release()
+cap = cv2.VideoCapture(0)
 
-if __name__ == "__main__":
-    main()
+if not cap.isOpened():
+    st.error("Error: Unable to access the webcam.")
+frame_width = 640
+frame_height = 480
+cap.set(3, frame_width)
+cap.set(4, frame_height)
+placeholder = st.empty()
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    if not ret:
+        st.error("Error: Unable to capture frame.")
+        break
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    placeholder.image(rgb_frame, channels='RGB')
+cap.release()
+
